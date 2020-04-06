b4553a22c457cd221e4ea3b64245b00f7fecabd5
Felipe Diet <felipe.oliveira@evup.com.br>
2020-04-06 08:25:23 -0300

    Ajustes

--- capy/spec/spec_helper.rb
+++ capy/spec/spec_helper.rb
@@ -16,22 +16,23 @@ RSpec.configure do |config|
 
   config.include Capybara::DSL
 
-  # # Manipula resolução
-  # config.before(:example) do
-  #   page.current_window.resize_to(1280, 800)
-  # end
+  # Manipula resolução
+  config.before(:example) do
+    page.current_window.resize_to(1920, 1080)
+  end
   
   
 # tira evidência no final do teste
   config.after(:example) do |e|
     nome = e.description.gsub(/[^A-Za-z0-9 ]/, '').tr(' ', '_')
-    page.save_screenshot('log/' + 'Erro_cenario_' + nome + '.png') if e.exception
+    page.save_screenshot('log/' + 'Erro_cenario_' + nome + '.png') #if e.exception
   end
 
 end
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
+  # config.default_driver = :selenium_chrome_headless # Executa em modo headless
   config.default_max_wait_time = 5 # tempo padrão que o sistema aguarda entre as execuções
   config.app_host = 'https://training-wheels-protocol.herokuapp.com' # URL padrão da aplicação, no before usar apenas o /página
   end
