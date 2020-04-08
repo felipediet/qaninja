e2912119d1fcd15e8cdbe2bbb23a507436739987
Felipe Diet <felipe.oliveira@evup.com.br>
2020-04-08 09:12:41 -0300

    Docker
    Comandos e ambiente

---
+++ Util/Docker.rb
@@ -0,0 +1,30 @@
+# Docker
+
+# Lista os containers ativos
+docker ps
+
+# Lista os containers
+docker ps -a
+
+# Ativa o conainer
+docker start nome_do_container
+
+# lista as imagens que foi dado pull
+docker images
+
+# Ambiente
+
+# Banco
+http://localhost:15432/browser/
+
+#  APIS
+
+# {"status":"NinjaFlix - API Gateway"}
+http://localhost:3000/
+
+# {"status": "NinjaFlix - Users API"}
+http://localhost:3001/
+
+# {"status": "NinjaFlix - Movies API"}
+http://localhost:3002/
+
